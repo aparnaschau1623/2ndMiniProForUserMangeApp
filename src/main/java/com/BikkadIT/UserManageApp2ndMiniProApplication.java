d77bfb4610b0f67a729c4633dabae9462067569a
SATYAJEET SHARDUL <aparnaschau1623@gamil.com>
2022-09-02 19:42:49 +0530

    2nd mini Project created

---
+++ src/main/java/com/BikkadIT/UserManageApp2ndMiniProApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManageApp2ndMiniProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManageApp2ndMiniProApplication.class, args);
+	}
+
+}
